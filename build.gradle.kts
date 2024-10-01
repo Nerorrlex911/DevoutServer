b393bd2af4b377792ef9d970aded5e78d13c1abf
Nerorrlex911 <nerorrlex911@outlook.com>
2024-10-01 21:32:01 +0800

    feat: EasyTerminal from BrokenDust

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("jvm") version "1.9.22"
+    id("com.github.johnrengelman.shadow") version "8.1.1"
 }
 
 group = "com.github.zimablue.devoutserver"
@@ -10,6 +11,7 @@ repositories {
     maven("https://jitpack.io")
     maven { url = uri("https://repo.tabooproject.org/repository/releases/") }
     maven { url = uri("https://oss.sonatype.org/content/repositories/snapshots") }
+    maven { url = uri("https://repo.hypera.dev/snapshots") }
 
 }
 
@@ -23,6 +25,13 @@ dependencies {
     compileOnly("com.electronwill.night-config:json:3.6.7")
     compileOnly("com.electronwill.night-config:hocon:3.6.7")
     implementation("com.electronwill.night-config:core-conversion:6.0.0")
+    //terminal
+    implementation("org.jline:jline-reader:3.25.0")
+    implementation("org.jline:jline-terminal:3.25.0")
+    implementation("org.jline:jline-terminal-jna:3.25.0")
+    implementation("org.tinylog:tinylog-api:2.7.0")
+    implementation("org.tinylog:tinylog-impl:2.7.0")
+    implementation("org.fusesource.jansi:jansi:2.4.1")
     //reflex
     // 本体
     implementation("org.tabooproject.reflex:analyser:1.0.23")
@@ -36,17 +45,26 @@ dependencies {
     compileOnly("com.google.guava:guava:21.0")
     //minestom
     // https://mvnrepository.com/artifact/net.minestom/minestom-snapshots
-    implementation("net.minestom:minestom-snapshots:12794d4263")
+    implementation("net.minestom:minestom-snapshots:d955f51899")
     implementation("com.github.Minestom:DependencyGetter:v1.0.1")
     implementation("dev.hollowcube:minestom-ce-extensions:1.2.0")
     //database
     implementation("com.zaxxer:HikariCP:4.0.3")
     //spark
     compileOnly("me.lucko:spark-api:0.1-SNAPSHOT")
+    //luckperms
+    implementation("me.lucko.luckperms:minestom:5.4-SNAPSHOT")
+
     implementation(fileTree("libs"))
 
 }
 
 tasks.test {
     useJUnitPlatform()
+}
+tasks.withType<Jar> {
+    dependsOn(tasks.shadowJar)
+    manifest {
+        attributes["Main-Class"] = "com.github.zimablue.devoutserver.MainKt"
+    }
 }
---
+++ src/main/kotlin/com/github/zimablue/devoutserver/internal/core/terminal/package-info.java
@@ -0,0 +1,6 @@
+package com.github.zimablue.devoutserver.internal.core.terminal;
+/*
+*
+* 土皇帝亲手铸做而成
+*
+* */
